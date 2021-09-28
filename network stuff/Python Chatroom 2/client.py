12026ca7c1142322544e2d6f12944e136db1de35
berserkware <berserkware@gmail.com>
2021-09-28 18:30:12 +1300

    created more efficient chatroom

--- network stuff/Python Chatroom 2/client.py
+++ network stuff/Python Chatroom 2/client.py
@@ -1,2 +1,35 @@
 import socket 
 import threading
+
+class Client():
+    def __init__(self, SERVER, PORT):
+        self.addr = (SERVER, PORT)
+
+    def Receive(self):
+        while True:
+            print(self.client.recv(2048).decode("utf-8"))
+
+    def Send(self, msg):
+        self.client.send(msg.encode("utf-8"))
+
+
+    def Start(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.addr)
+
+
+        thread = threading.Thread(target=self.Receive, args=())
+        thread.start()
+
+        while True:
+            name = input("What is your username - ")
+            if name != "":
+                self.Send(name)
+                break
+
+        while True:
+            msg = input()
+            self.Send(msg)
+
+client = Client(socket.gethostbyname(socket.gethostname()), 5050)
+client.Start()
--- network stuff/Python Chatroom 2/server.py
+++ network stuff/Python Chatroom 2/server.py
@@ -2,19 +2,54 @@ import socket
 import threading
 
 class Server():
-    def __init__(self, ADDR, PORT):
-        self.addr = ADDR
-        self.port = PORT
-    
-    def Start_Server(self):
-        pass
-
-    def Close_Server(self):
-        pass
+    def __init__(self, SERVER, PORT):
+        self.addr = (SERVER, PORT)
 
     def Handle_Client(self, conn, addr):
-        pass
+        username = conn.recv(1024).decode("utf-8")
+
+        self.Send(f"[CONNECTION] {username} has joined the room")
+
+        while True:
+            try:
+                msg = conn.recv(1024).decode("utf-8")
+            except:
+                conn.close()
+                self.clients.remove(conn)
+
+                self.Send(f"[CONNECTION] {username} has left the room")
+
+                break
+
+            if msg != "":
+                self.Send(f"[{username}] {msg}")
+        
 
     def Send(self, msg):
-        pass
+        for i in self.clients:
+            i.send(str(msg).encode("utf-8"))
+        print(msg)
+
+    
+    def Start_Server(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
+
+        self.clients = []
+
+        print("[SERVER] Server is starting")
+        self.server.bind(self.addr)
+
+        self.server.listen()
+        print(f"[SERVER] Server is listning on {self.addr}")
+
+        while True:
+            conn, addr = self.server.accept()
+            self.conn = conn
+            self.clients.append(conn)
+            thread = threading.Thread(target=self.Handle_Client, args=(conn, addr))
+            thread.start()
+            print(threading.activeCount() - 1)
         
+
+server = Server(socket.gethostbyname(socket.gethostname()), 5050)
+server.Start_Server()
